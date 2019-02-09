fa585c1bf88e01ba4b305959f0c9dd3f552ad4e7
Daniel <josedanielchinchilla@gmail.com>
2019-02-08 21:23:20 -0600

    added main menu

--- ejercicios/src/sv/edu/udb/Ejercicios.java
+++ ejercicios/src/sv/edu/udb/Ejercicios.java
@@ -5,12 +5,32 @@
  */
 package sv.edu.udb;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author josed
  */
 public class Ejercicios {
+    private static final String[] OPCIONES = {"SALIR","SALUDO","CAJERO","MEDIATECA"};
     public static void main(String[] args) {
-        
+        int seleccion = 0;
+        do {
+            seleccion= JOptionPane.showOptionDialog(null, "Seleccione una operacion", "seleccion", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, OPCIONES, OPCIONES[0]);
+            switch(seleccion){
+                case -1:
+                    JOptionPane.showMessageDialog(null, "Seleccione una operacion valida");
+                    break;
+                case 0:
+                    JOptionPane.showMessageDialog(null, "Gracias por usar la calculadora");
+                    break;
+                case 1://ejercicio 1
+                    break;
+                case 2://ejercicio 2
+                    break;
+                case 3://ejercicio 3
+                    break;            
+            }
+        }while(seleccion!=0);
     }
 }
