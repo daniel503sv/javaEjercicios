aad98c2fdad4d69160d0bf2f831f47efbfe66dae
Daniel <daniel.chinchilla@kadevjo.com>
2019-02-15 14:17:04 -0600

    cleaning code

--- ejercicios/src/sv/edu/udb/tercero/Tercero.java
+++ ejercicios/src/sv/edu/udb/tercero/Tercero.java
@@ -1,4 +1,3 @@
-
 package sv.edu.udb.tercero;
 
 import java.io.IOException;
@@ -11,26 +10,28 @@ import javax.swing.JOptionPane;
 *              Oscar Aragón
 *              Rafael Lara
 *              Daniel Choriego
-*/
+ */
 public class Tercero {
-    private static final String[] OPERACIONES = {"SALIR","AGREGAR","LISTAR","BORRAR","BUSCAR"};
+
+    private static final String[] OPERACIONES = {"SALIR", "AGREGAR", "LISTAR", "BORRAR", "BUSCAR"};
     private static Mediateca mediateca;
+
     public static void main(String[] args) {
         mediateca = new Mediateca();
         boolean continuar = true;
-        int operacion =0;
+        int operacion = 0;
 
         try {
             mediateca.cargar(); //se carga el xml
         } catch (IOException ex) { //no se pudo cargar el xml
             System.out.println(ex);
         }
-        
-        do{
-            operacion = JOptionPane.showOptionDialog(null, "Mediateca seleccione una opcion"
-                      , "MEDIATECA", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION
-                      , null, OPERACIONES, OPERACIONES[0]);
-            switch(operacion){
+
+        do {
+            operacion = JOptionPane.showOptionDialog(null, "Mediateca seleccione una opcion",
+                     "MEDIATECA", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION,
+                     null, OPERACIONES, OPERACIONES[0]);
+            switch (operacion) {
                 case 0:
                     continuar = false;
                     break;
@@ -47,24 +48,12 @@ public class Tercero {
                     mediateca.mostrar();
                     break;
             }
-        }while(continuar);
-        
-        
-        //creacion
-        //mediateca.ingresarNuevoLibro("titulo libro","editorial libro",25,"autor",500, "ISBN?", 1999);
-        //mediateca.ingresarNuevoLibro("titulo libro 2","editorial libro 2",25,"autor 2",500, "ISBN?", 1999);
-   
-        //busqueda
-        //Libro libro = (Libro) mediateca.buscarMaterial("LIB00000");
-        //mediateca.borrarMaterial("LIB00000");
-        
-        try {
-            mediateca.guardar(); //se guarda al xml
-        } catch (IOException ex) {
-            System.out.println(ex);
-        }
+            try {
+                mediateca.guardar(); //se guarda al xml
+            } catch (IOException ex) {
+                System.out.println(ex);
+            }
+        } while (continuar);
     }
-    
-   
 
 }
